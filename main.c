36e09045cb9aaf5689eb0713ce82d3d88e3805fb
Sheeper <55878551+ImSheeper@users.noreply.github.com>
2021-11-10 12:11:54 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char tn;
+    do{
+        int n;
+
+        printf("Podaj n: \n");
+        scanf("%d", &n);
+
+        if(n >= 1 && n <= 15) {
+            for(int i = 1; i <= n; i++) {
+                printf("\n");
+
+                for(int j = n; j >= i; j--) {
+                    printf(" ");
+
+                    if(j == i) {
+                        for (int g = 1; g <= i + j - 1; g++) {
+                            printf("*");
+                        }
+                    }
+                }
+            }
+        }
+        else{
+            printf("Zla wartosc \n");
+        }
+
+        printf("\nPowtorzyc program? t/n \n");
+        scanf(" %c", &tn);
+
+    }while(tn!='n');
+
+    return 0;
+}
